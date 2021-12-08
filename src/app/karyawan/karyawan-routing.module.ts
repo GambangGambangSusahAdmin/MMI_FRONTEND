fdac860e167d689c611a061d99e5007dc4eee51b
anugrah <z.putra.anugrah@gmail.com>
2021-12-08 15:56:08 +0700

    tambahan dikit

--- src/app/karyawan/karyawan-routing.module.ts
+++ src/app/karyawan/karyawan-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KaryawanComponent } from './karyawan.component';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./list/list.module').then(m => m.ListModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./list/list.module').then(m => m.ListModule) }, { path: 'add', loadChildren: () => import('./add/add.module').then(m => m.AddModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
